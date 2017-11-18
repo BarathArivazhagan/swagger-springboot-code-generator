160ae799c5700d285b7bbd521df76890aecadecc
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-11-18 11:09:42 +0530

    code commit

--- src/main/java/com/barath/app/model/ClassModel.java
+++ src/main/java/com/barath/app/model/ClassModel.java
@@ -2,24 +2,43 @@ package com.barath.app.model;
 
 import java.util.Set;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 
-
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
-@AllArgsConstructor
 public class ClassModel {
 	
 	
 	private String name;
 	
 	private Set<MethodModel> methods;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<MethodModel> getMethods() {
+		return methods;
+	}
+
+	public void setMethods(Set<MethodModel> methods) {
+		this.methods = methods;
+	}
+
+	public ClassModel(String name, Set<MethodModel> methods) {
+		super();
+		this.name = name;
+		this.methods = methods;
+	}
+
+	public ClassModel() {
+		super();
+		
+	}
+	
+	
+	
 
 }
